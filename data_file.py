d28ab9555a4f76b1a587edf8cdd1b899cc748f71
Công Nguyễn <congnd24@gmail.com>
2020-03-20 16:12:13 +0700

    first commit

---
+++ data_file.py
@@ -0,0 +1,5 @@
+qc_lead = "Công"
+qc_en = ["Hoa", "Huyền", "Uyên"]
+team_qc = ["Hoa", "Huyền", "Uyên"]
+qc_lead_role = "QC Leader"
+qc_en_role = "QC Engineer"
---
+++ main.py
@@ -0,0 +1,9 @@
+name_1 = input("Hãy nhập tên của bạn: ")
+age_1 = int(input("Bao tuổi?: "))
+name_2 = input("Hãy nhập tên bạn của bạn: ")
+age_2 = int(input("Bao tuổi?: "))
+result = name_1 + " đẹp trai, còn " + name_2 + " xấu trai vãi loz, quỳ xuống con trai ạ"
+
+print(result)
+print(name_1 + " hơn " + name_2 + " " + str(age_1 - age_2) + " tuổi")
+
