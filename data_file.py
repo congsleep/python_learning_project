194a34f98f0d762fa2f8154210839dd40ea685da
congn <congnd24@gmail.com>
2020-03-21 02:54:53 +0700

    win push

--- data_file.py
+++ data_file.py
@@ -1,5 +1,6 @@
 qc_lead = "Công"
 qc_en = ["Hoa", "Huyền", "Uyên"]
-team_qc = ["Hoa", "Huyền", "Uyên"]
+team_qc = ["Công", "Hoa", "Huyền", "Uyên"]
+team_age = [29, 25, 25, 23]
 qc_lead_role = "QC Leader"
 qc_en_role = "QC Engineer"
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-name_1 = input("Hãy nhập tên của bạn: ")
-age_1 = int(input("Bao tuổi?: "))
-name_2 = input("Hãy nhập tên bạn của bạn: ")
-age_2 = int(input("Bao tuổi?: "))
-result = name_1 + " đẹp trai, còn " + name_2 + " xấu trai vãi loz, quỳ xuống con trai ạ"
+import data_file
+# name_1 = input("Hãy nhập tên của bạn: ")
+# age_1 = int(input("Bao tuổi?: "))
+# name_2 = input("Hãy nhập tên bạn của bạn: ")
+# age_2 = int(input("Bao tuổi?: "))
+# result = name_1 + " đẹp trai, còn " + name_2 + " xấu trai vãi loz, quỳ xuống con trai ạ"
+#
+# print(result)
+# print(name_1 + " hơn " + name_2 + " " + str(age_1 - age_2) + " tuổi")
 
-print(result)
-print(name_1 + " hơn " + name_2 + " " + str(age_1 - age_2) + " tuổi")
+user_input_name = input("Nhập tên của bạn: ")
+def checktuoi():
+    if user_input_name in data_file.team_qc[0:3]:
+        # print(str(data_file.team_qc[0:3]) + " hiện đang " + str(data_file.team_age[0]) + " tuổi")
+        print("User này có trong team")
+    else:
+        print("Không tìm thấy tên này trong danh sách!")
 
-123
+checktuoi()
