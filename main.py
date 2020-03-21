3ae29ebcbf0d0ddea95c0984e1d4fdc0980d7aa4
congn <congnd24@gmail.com>
2020-03-21 21:04:40 +0700

    update 21/3/2020

--- main.py
+++ main.py
@@ -8,12 +8,57 @@ import data_file
 # print(result)
 # print(name_1 + " hơn " + name_2 + " " + str(age_1 - age_2) + " tuổi")
 
-user_input_name = input("Nhập tên của bạn: ")
-def checktuoi():
-    if user_input_name in data_file.team_qc[0:3]:
-        # print(str(data_file.team_qc[0:3]) + " hiện đang " + str(data_file.team_age[0]) + " tuổi")
-        print("User này có trong team")
-    else:
-        print("Không tìm thấy tên này trong danh sách!")
+#---------------------------------------------------
+# user_input_name = input("Nhập tên của bạn: ")
+# def checktuoi():
+#     if user_input_name in data_file.team_qc[0:3]:
+#         # print(str(data_file.team_qc[0:3]) + " hiện đang " + str(data_file.team_age[0]) + " tuổi")
+#         print("User này có trong team")
+#     else:
+#         print("Không tìm thấy tên này trong danh sách!")
+#
+# checktuoi()
 
-checktuoi()
+#---------------------------------------------------
+# def cube(num):
+#     # result = num*num*num
+#     # print(result)
+#     return num*num*num
+#
+# print(cube(5))
+
+#---------------------------------------------------
+# def max_num(num_1, num_2, num_3):
+#     if num_1 >= num_2 and num_1 >= num_3:
+#         print(str(num_1) + " là số lớn nhất")
+#     elif num_2 >= num_1 and num_2 >= num_3:
+#         print(str(num_2) + " là số lớn nhất")
+#     else:
+#         print(str(num_3) + " là số lớn nhất")
+#
+# num1 = input("Nhập số thứ 1: ")
+# num2 = input("Nhập số thứ 2: ")
+# num3 = input("Nhập số thứ 3: ")
+#
+# max_num(num1, num2, num3)
+
+#---------------------------------------------------
+# monthConversion = {
+#     1: "Tháng Một",
+#     2: "Tháng Hai",
+#     3: "Tháng Ba",
+#     4: "Tháng Bốn",
+#     5: "Tháng Năm",
+#     6: "Tháng sáu",
+#     7: "Tháng Bảy",
+#     8: "Tháng Tám",
+#     9: "Tháng Chín",
+#     10: "Tháng Mười",
+#     11: "Tháng Mười một",
+#     12: "Tháng Mười hai"
+# }
+#
+# print(monthConversion[2])
+# print(monthConversion.get(11))
+
+#---------------------------------------------------
